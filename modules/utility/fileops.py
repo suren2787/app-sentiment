ec8db7a8b97d78fb945855b8b2cb3623be576c5e
SurenPlanet <surenplanet@gmail.com>
2024-12-02 23:25:14 +0800

    gpt manual comments added

--- modules/utility/fileops.py
+++ modules/utility/fileops.py
@@ -8,5 +8,5 @@ def renamefile(file_name_str, backup_filename_str):
 
 # # Writing to sample.json
 def savefile(file_name_str, json_object):
-    with open(file_name_str, "w") as outfile:
+    with open(file_name_str, "w",encoding="utf-8") as outfile:
         outfile.write(json_object)
--- sentimeter/insights/summarization.py
+++ sentimeter/insights/summarization.py
@@ -10,7 +10,7 @@ from modules.utility import fileops
 
 print('[INFO] summarization - Start.')
 reviews_file = 'results/summary.json'
-comments_Current=""
+comments_current=""
 comments_current_1=""
 comments_current_2=""
 current_month = datetime.now().month
@@ -30,7 +30,7 @@ with open(reviews_file, 'r') as ifh:
 
         if(sentiment=='NEGATIVE' and reviewdate.year==current_year):                                                                      
             if(reviewdate.month==current_month):
-                comments_Current = comments_Current + "\n" + text #comments.join(text)
+                comments_current = comments_Current + "\n" + text #comments.join(text)
             if(reviewdate.month==current_month-1):
                 comments_current_1 = comments_current_1 + "\n" + text 
             if(reviewdate.month==current_month-2):
@@ -39,31 +39,37 @@ print('[INFO] summarization - Filtered comments. Sentiment analysis is in progre
 #get summary of comments
 from transformers import pipeline
 
-summarizer = pipeline(task="summarization")
-result = summarizer(comments_current_1)
-result2 = summarizer(comments_current_2)
-print("-------------------------------------------------")
-print(comments_current_1)
-print("-------------------------------------------------")
-print(result)
-print("-------------------------------------------------")
-print(comments_current_2)
-print("-------------------------------------------------")
-print(result2)
-print("-------------------------------------------------")
-print('[INFO] summarization - sentiments analyzed. Saving File as json')
+# summarizer = pipeline(task="summarization")
+# result = summarizer(comments_current_1)
+# result2 = summarizer(comments_current_2)
+# print("-------------------------------------------------")
+# print(comments_current_1)
+# print("-------------------------------------------------")
+# print(result)
+# print("-------------------------------------------------")
+# print(comments_current_2)
+# print("-------------------------------------------------")
+# print(result2)
+# print("-------------------------------------------------")
+# print('[INFO] summarization - sentiments analyzed. Saving File as json')
 
-# #save sentiments
-# #construct filename
-# time_str = time.strftime("%Y%m%d-%H%M%S")
-# file_name_str = "results/reviews-sentimeter.json"
-# backup_filename_str="results/reviews-sentimeter-backup-"+time_str+".json"
+#save sentiments
+#construct filename
+time_str = time.strftime("%Y%m%d-%H%M%S")
+file_name_str = "results/reviews-current.txt"
+backup_filename_str="results/reviews-current-backup-"+time_str+".txt"
+file_name_str_lastmonth = "results/reviews-current-1.txt"
+backup_filename_str_lastmonth="results/reviews-current-1-backup-"+time_str+".txt"
+file_name_str_lastbefore = "results/reviews-current-2.txt"
+backup_filename_str_lastbefore ="results/reviews-current-2-backup-"+time_str+".txt"
 
-# #check if file exists
-# fileops.renamefile(file_name_str,backup_filename_str)
+#check if file exists
+fileops.renamefile(file_name_str,backup_filename_str)
+fileops.renamefile(file_name_str_lastmonth,backup_filename_str_lastmonth)
+fileops.renamefile(file_name_str_lastbefore,backup_filename_str_lastbefore)
 
-# # # Writing to sample.json
-# json_object = json.dumps(result, indent=4,default=str)
-# fileops.savefile(file_name_str,json_object)
-
-# print('[INFO] summarization - All steps completed')
+# # Writing to sample.json
+fileops.savefile(file_name_str,comments_current)
+fileops.savefile(file_name_str_lastmonth,comments_current_1)
+fileops.savefile(file_name_str_lastbefore,comments_current_2)
+print('[INFO] summarization - All steps completed')
