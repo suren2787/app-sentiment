7adcbb3370efcbaa2f904be81ea8ccd51f32ca1d
SurenPlanet <surenplanet@gmail.com>
2025-02-10 08:16:33 +0800

    updated summarization

--- sentimeter/insights/summarization.py
+++ sentimeter/insights/summarization.py
@@ -16,6 +16,10 @@ comments_current_2=""
 current_month = datetime.now().month
 current_year = datetime.now().year
 
+comments_array=[]
+comments_array_1=[]
+comments_array_2=[]
+
 print('[INFO] summarization - Loaded normalized reviews. Filtering comments from Json.')
 #Open file and filter only comments
 with open(reviews_file, 'r') as ifh:
@@ -31,19 +35,25 @@ with open(reviews_file, 'r') as ifh:
         if(sentiment=='NEGATIVE' and reviewdate.year==current_year):                                                                      
             if(reviewdate.month==current_month):
                 comments_current = comments_current + "\n" + text #comments.join(text)
+                comments_array.append(text)
             if(reviewdate.month==current_month-1):
                 comments_current_1 = comments_current_1 + "\n" + text 
+                comments_array_1.append(text)
             if(reviewdate.month==current_month-2):
                 comments_current_2 = comments_current_2 + "\n" + text 
-print('[INFO] summarization - Filtered comments. Sentiment analysis is in progress')
+                comments_array_2.append(text)
+print('[INFO] summarization - Filtered comments. Summary analysis is in progress')
 #get summary of comments
 from transformers import pipeline
 
 classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
-categories = ["UI/UX", "Performance", "Bugs", "Feature Requests", "Customer Support"]
-category_result = classifier(comments_current_1, candidate_labels=categories)
-print("Category:", category_result['labels'][0])
-print("Category:", category_result)
+categories = ["UI/UX", "Unstable App", "Unfair Charges", "Slow", "Customer Service"]
+category_result = classifier(comments_array_1, candidate_labels=categories)
+# Print the results
+for i, result in enumerate(category_result):
+    print(f"Feedback: {comments_array_1[i]}")
+    print(f"Predicted Category: {result['labels'][0]} (Score: {result['scores'][0]:.2f})")
+    print()
 
 # summarizer = pipeline(task="summarization")
 # result = summarizer(comments_current_1)
